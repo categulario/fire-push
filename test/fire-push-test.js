eda63d054d0173b46d766929e8eae5147e20b834
DezkaReid <elmaildeldezkareid@gmail.com>
2017-04-08 17:06:33 -0500

    #Fire-Push Test #sendMessage message method must be fullfilled

--- test/fire-push-test.js
+++ test/fire-push-test.js
@@ -3,8 +3,9 @@ const expect = chai.expect
 const sinon = require('sinon')
 chai.use(require("sinon-chai"))
 chai.use(require('chai-as-promised'))
+const Promise = require('bluebird')
 const FirePush = require('..').default
-const RequestHelper = require('../dist/src/request-helper').default
+const RequestHelper = require('../dist/src/request-helper').default;
 
 describe('#Fire-Push Test', function () {
 	it('Api key equal when is setted in constructor and when is getted', function () {
@@ -13,27 +14,17 @@ describe('#Fire-Push Test', function () {
 		expect(apiKey).to.be.equal(firePush.configRequest.apiKey)
 	})
 
-	describe('#sendMessage', function(){
-		let firePush;
-		let sandbox;
-
-		before(() => {
-			let apiKey = '17272829823';
-			firePush = new FirePush(apiKey);
-		});
+	describe('#sendMessage', function() {
+		let apiKey
+		let firePush
+		let sandbox
+		let message
 
 		beforeEach(() => {
 			sandbox = sinon.sandbox.create();
-		});
-
-		afterEach(()=>{
-			sandbox.restore();
-		});
-
-		it('message method must be fullfilled', function () {
-			let requestSenderStub = sandbox.stub(RequestHelper, 'sendRequest');
-			requestSenderStub.returns(Promise.resolve({ message_id : 7926694608618042676 }));
-			let message = {
+			firePush = new FirePush({apiKey});
+			apiKey = '17272829823';
+			message = {
 				notification : {
 					title : 'Ey',
 					body : 'Make push notifications great again',
@@ -41,8 +32,16 @@ describe('#Fire-Push Test', function () {
 				},
 				to : 'topics/user_1'
 			};
+		});
 
-			let promiseSended = firePush.sendMessage(message);
+		afterEach(()=>{
+			sandbox.restore();
+		});
+
+		it('message method must be fullfilled', function () {
+			let requestSenderStub = sandbox.stub(RequestHelper.prototype, 'sendRequest');
+			requestSenderStub.returns(Promise.resolve({ message_id : "1929292"}));
+			let promiseSended = firePush.sendRequest(message);
 			expect(requestSenderStub).to.have.been.called;
 			expect(promiseSended).to.be.fulfilled;
 			expect(promiseSended).to.eventually.have.property("message_id");
