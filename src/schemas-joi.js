83a6ae2202a3fb6726f2e08d4aa8e53dac519c14
DezkaReid <elmaildeldezkareid@gmail.com>
2017-04-08 05:23:01 -0500

    Added schemas to messages and data

--- src/schemas-joi.js
+++ src/schemas-joi.js
@@ -1,11 +1,42 @@
 import Joi from 'joi';
 
+const notification = {
+	title : Joi.string(),
+	body : Joi.string(),
+	icon : Joi.string(),
+	sound : Joi.string(),
+	badge : Joi.string(),
+	tag	: Joi.string(),
+	color : Joi.string(),
+	click_action : Joi.string(),
+	body_loc_key : Joi.string(),
+	body_loc_args : Joi.array().items(Joi.string()),
+	title_loc_key : Joi.string(),
+	title_loc_args : Joi.array().items(Joi.string()),
+};
+
+const data = Joi.object().pattern(/.*/, Joi.string());
+
+const message = {
+	condition : Joi.string(),
+	registration_ids : Joi.array().items(Joi.string()),
+	collapse_key : Joi.string(),
+	priority : Joi.string(),
+	content_available : Joi.boolean(),
+	delay_while_idle : Joi.boolean(),
+	time_to_live : Joi.number(),
+	restricted_package_name	: Joi.string(),
+	dry_run	: Joi.boolean(),
+	to : Joi.string(),
+	notification : Joi.object().keys(notification),
+	data : data
+};
+
+
+
 const schemas = {
-	notification : {
-		title: Joi.string(),
-		body: Joi.string(),
-		sound: Joi.string()
-	}
+	notification : notification,
+	message : message,	
 }
 export default {
 	loadSchema : function (schemaName) {
